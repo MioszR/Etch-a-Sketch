aeb61a4c0427cb99e75127fc2ba55ed89a995e2d
MioszR <99885857+MioszR@users.noreply.github.com>
2023-07-17 22:07:08 +0200

    Create description, and add few things

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
+//Choose container in html file
 let container = document.querySelector('#container');
 
-
+//Create and add left side section
 let leftSide = document.createElement('div');
 leftSide.classList.add('left-side');
 let eraser = document.createElement('button');
-eraser.classList.add('eraser');
-eraser.classList.add('button');
-eraser.textContent = 'Eraser';
+eraser.classList.add('eraser', 'button');
+eraser.textContent = 'Erase';
 let clear = document.createElement('button');
-clear.classList.add('clear');
-clear.classList.add('button');
+clear.classList.add('clear', 'button');
 clear.textContent = 'Clear';
 leftSide.appendChild(eraser);
 leftSide.appendChild(clear);
 container.appendChild(leftSide);
 
+//Create center section
 let center = document.createElement('div');
 center.classList.add('center');
 center.textContent = "Hello";
 container.appendChild(center);
 
+//Create left side section
 let rightSide = document.createElement('div');
 rightSide.classList.add('right-side');
-let
+let color = document.createElement('button');
+color.classList.add('color', 'button');
+color.textContent = 'Color';
+let black = document.createElement('button');
+black.classList.add('black', 'button');
+black.textContent = 'Black';
+rightSide.appendChild(color);
+rightSide.appendChild(black);
+container.appendChild(rightSide);
+
+//Connect buttons
+let eraseButton = document.querySelector('.eraser');
+let clearButton = document.querySelector('.clear');
+let color = document.querySelector('.color');
+let black = document.querySelector('black');
+
+//Add events to the buttons
+clearButton.addEventListener('click', () => {
+    center.style.backgroundColor = "#fff";
+})
