fd7a47e4af59ad1d38c87562dd128a1510b591bc
MioszR <99885857+MioszR@users.noreply.github.com>
2023-07-20 17:53:19 +0200

    Create function to make a grid

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-let currentValue = 32;
+document.addEventListener('DOMContentLoaded', function(){
+    console.log("hi")
+})
+
+
+let currentValue = 3;
 
 //Connect grid place
-center = document.querySelector('.center');
 
 //Connect buttons
 let eraseButton = document.querySelector('.eraser');
@@ -10,8 +14,22 @@ let colorButton = document.querySelector('.color');
 let blackButton = document.querySelector('black');
 let sizer = document.querySelector('.sizer-button');
 let value = document.querySelector('.value');
-console.log(sizer.value)
 
+function createCenter(currentValue) {
+    let center = document.querySelector('.center');
+    center.style.gridTemplateColumns = `repeat(${currentValue}, 1fr)`
+    center.style.gridTemplateRows = `repeat(${currentValue}, 1fr)`
+
+    let divsNum = currentValue * currentValue
+
+    for(let i = 0; i < divsNum; i++) {
+        let div = document.createElement('div');
+        div.style.backgroundColor = '#fff';
+        center.appendChild(div)
+    }
+}
+
+console.log(sizer.value)
 //Add events to the buttons
 clearButton.addEventListener('click', () => {
     center.style.backgroundColor = "#fff";
@@ -19,3 +37,23 @@ clearButton.addEventListener('click', () => {
 sizer.addEventListener('mousemove', () => {
     value.textContent = `${sizer.value} x ${sizer.value}`
 })
+
+sizer.addEventListener('mouseup', () => {
+    gridReset()
+    setSizerValue()
+    createCenter(currentValue)
+
+    // gridMaker()
+})
+
+let setSizerValue = () => {
+    currentValue = sizer.value;
+}
+
+
+
+let gridReset = () => {
+    createCenter(32)
+}
+
+
